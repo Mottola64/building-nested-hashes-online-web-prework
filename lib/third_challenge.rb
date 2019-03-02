f04e86c00c7376de95dbdb54dc3cdd7604adc715
Michael Mottola <mottola64@gmail.com>
2019-03-02 23:15:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,15 +2,21 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "53"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {
+          
+        }]
    }, 
    :capulet => {
       :patriarch => {
